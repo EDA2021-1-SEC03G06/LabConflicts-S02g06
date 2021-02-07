93eb9cb5be037ee6d65494d39347c90e965a4804
fuckeverione <k.correal@uniandes.edu.co>
2021-02-07 18:59:29 -0500

    est1 cambios

--- App/view.py
+++ App/view.py
@@ -42,6 +42,8 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
+    print("3- Cargar Book-Tags!")
+
     # TO-DO: Modificación de Est-1 en el Lab 2
     print("0- Salir")
 
@@ -64,6 +66,8 @@ def loadBookTags():
     Cargar los Tags de libros
     """
     # TO-DO: Modificación de Est-1 en el Lab 2
+    return controller.loadBooks('GoodReads/book_tags-small.csv')
+
 
 
 """
@@ -78,6 +82,7 @@ while True:
         print('Total de libros cargados: ' + str(lt.size(books)))
 
         # TO-DO: Modificación de Est-1 en el Lab 2
+        print('Primer libro cargado: ' + str(lt.firstElement(books)))
 
 
         # TO-DO: Modificación de Est-2 en el Lab 2
